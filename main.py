a9b6f5b44adc3b8236ca24621aa6290b7916be7a
Sam120204 <75916625+Sam120204@users.noreply.github.com>
2024-07-31 16:32:55 -0400

    update main

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from utils import read_video, save_video
 from trackers import Tracker
-
+import cv2
 def main():
     #read videos
     video_frames = read_video('input_videos/08fd33_4.mp4')
@@ -10,6 +10,18 @@ def main():
     tracks = tracker.get_object_tracks(video_frames,
                                        read_from_stub=True,
                                        stub_path='stubs/track_stubs.pk1')
+    
+    # save cropped images of a player
+    for track_id, player in tracks['players'][0].items():
+        bbox = player['bbox']
+        frame = video_frames[0]
+        
+        # crop bbox from frame
+        cropped_image = frame[int(bbox[1]):int(bbox[3]), int(bbox[0]):int(bbox[2])]
+        
+        # save the cropped image
+        cv2.imwrite(f'output_videos/cropped_image.jpg', cropped_image)
+        break
     
     # Draw Output
     ## Draw Object Tracks
